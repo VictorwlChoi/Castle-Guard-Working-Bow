8a5445a51309b55ff6cc6a2ce13f5d8dffd0a7aa
VictorwlChoi <vncvrVictorChoi@Outlook.com>
2017-05-04 22:48:37 -0700

    Added Visual Studio Project Files

---
+++ CastleGuard/vive_archery_ue4-master/Intermediate/Build/Win64/UE4Editor/Development/vive_archery_ue4/PCH.vive_archery_ue4.h.cpp
@@ -0,0 +1,1 @@
+#include "E:\vive_archery_ue4-master\vive_archery_ue4-master\Source\vive_archery_ue4\vive_archery_ue4.h"
---
+++ CastleGuard/vive_archery_ue4-master/Intermediate/Build/Win64/UE4Editor/Development/vive_archery_ue4/vive_archery_ue4.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_VIVE_ARCHERY_UE4_H__
+#define __AUTO_VIVE_ARCHERY_UE4_H__
+//Last Write: 5/5/2017 4:28:07 AM +00:00
+#include "E:\vive_archery_ue4-master\vive_archery_ue4-master\Source\vive_archery_ue4\vive_archery_ue4.h"
+#endif//__AUTO_VIVE_ARCHERY_UE4_H__
