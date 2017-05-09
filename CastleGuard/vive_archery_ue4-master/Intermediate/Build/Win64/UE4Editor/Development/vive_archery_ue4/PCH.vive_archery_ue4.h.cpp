972827228eb17d3164841635ccac89d00e7887ae
VictorwlChoi <vncvrvictorchoi@outlook.com>
2017-05-08 20:06:11 -0700

    Added Gravity to Arrow

--- CastleGuard/vive_archery_ue4-master/Intermediate/Build/Win64/UE4Editor/Development/vive_archery_ue4/PCH.vive_archery_ue4.h.cpp
+++ CastleGuard/vive_archery_ue4-master/Intermediate/Build/Win64/UE4Editor/Development/vive_archery_ue4/PCH.vive_archery_ue4.h.cpp
@@ -1,1 +1,1 @@
-#include "E:\vive_archery_ue4-master\vive_archery_ue4-master\Source\vive_archery_ue4\vive_archery_ue4.h"
+#include "C:\Users\StudentUser\Documents\Github\272072\Castle-Guard-Working-Bow\CastleGuard\vive_archery_ue4-master\Source\vive_archery_ue4\vive_archery_ue4.h"
--- CastleGuard/vive_archery_ue4-master/Intermediate/Build/Win64/UE4Editor/Development/vive_archery_ue4/vive_archery_ue4.h
+++ CastleGuard/vive_archery_ue4-master/Intermediate/Build/Win64/UE4Editor/Development/vive_archery_ue4/vive_archery_ue4.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_VIVE_ARCHERY_UE4_H__
 #define __AUTO_VIVE_ARCHERY_UE4_H__
-//Last Write: 5/5/2017 4:28:07 AM +00:00
-#include "E:\vive_archery_ue4-master\vive_archery_ue4-master\Source\vive_archery_ue4\vive_archery_ue4.h"
+//Last Write: 5/9/2017 1:43:54 AM +00:00
+#include "C:\Users\StudentUser\Documents\Github\272072\Castle-Guard-Working-Bow\CastleGuard\vive_archery_ue4-master\Source\vive_archery_ue4\vive_archery_ue4.h"
 #endif//__AUTO_VIVE_ARCHERY_UE4_H__
