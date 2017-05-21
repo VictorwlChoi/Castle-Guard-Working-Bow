4626b698e0f9cb962aab6b364f2adf5d0b6bf6a9
VictorwlChoi <vncvrvictorchoi@outlook.com>
2017-05-20 17:12:25 -0700

    Bow Rotation Glitch solved

--- CastleGuard/vive_archery_ue4-master/Intermediate/Build/Win64/UE4Editor/Development/vive_archery_ue4/vive_archery_ue4.h
+++ CastleGuard/vive_archery_ue4-master/Intermediate/Build/Win64/UE4Editor/Development/vive_archery_ue4/vive_archery_ue4.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_VIVE_ARCHERY_UE4_H__
 #define __AUTO_VIVE_ARCHERY_UE4_H__
-//Last Write: 5/16/2017 1:56:47 AM +00:00
+//Last Write: 5/20/2017 9:16:36 PM +00:00
 #include "C:\Users\StudentUser\Documents\GitHub\272072\Castle-Guard-Working-Bow\CastleGuard\vive_archery_ue4-master\Source\vive_archery_ue4\vive_archery_ue4.h"
 #endif//__AUTO_VIVE_ARCHERY_UE4_H__
