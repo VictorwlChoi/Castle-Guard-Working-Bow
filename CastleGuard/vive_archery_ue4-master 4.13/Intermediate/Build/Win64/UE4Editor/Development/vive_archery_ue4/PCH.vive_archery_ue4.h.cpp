33b168bb6afb29b4d5e8fbf919f163f53a242d99
VictorwlChoi <vncvrvictorchoi@outlook.com>
2017-06-01 21:57:34 -0700

    Adjusted Bow Trajectory, added trails to Arrows

--- CastleGuard/vive_archery_ue4-master 4.13/Intermediate/Build/Win64/UE4Editor/Development/vive_archery_ue4/PCH.vive_archery_ue4.h.cpp
+++ CastleGuard/vive_archery_ue4-master 4.13/Intermediate/Build/Win64/UE4Editor/Development/vive_archery_ue4/PCH.vive_archery_ue4.h.cpp
@@ -1,1 +1,1 @@
-#include "C:\Users\StudentUser\Documents\GitHub\272072\Castle-Guard-Working-Bow\CastleGuard\vive_archery_ue4-master 4.13\Source\vive_archery_ue4\vive_archery_ue4.h"
+#include "E:\CGLD\Castle-Guard-Working-Bow\CastleGuard\vive_archery_ue4-master 4.13\Source\vive_archery_ue4\vive_archery_ue4.h"
--- CastleGuard/vive_archery_ue4-master 4.13/Intermediate/Build/Win64/UE4Editor/Development/vive_archery_ue4/vive_archery_ue4.h
+++ CastleGuard/vive_archery_ue4-master 4.13/Intermediate/Build/Win64/UE4Editor/Development/vive_archery_ue4/vive_archery_ue4.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_VIVE_ARCHERY_UE4_H__
 #define __AUTO_VIVE_ARCHERY_UE4_H__
-//Last Write: 5/30/2017 1:44:12 AM +00:00
-#include "C:\Users\StudentUser\Documents\GitHub\272072\Castle-Guard-Working-Bow\CastleGuard\vive_archery_ue4-master 4.13\Source\vive_archery_ue4\vive_archery_ue4.h"
+//Last Write: 6/2/2017 2:02:21 AM +00:00
+#include "E:\CGLD\Castle-Guard-Working-Bow\CastleGuard\vive_archery_ue4-master 4.13\Source\vive_archery_ue4\vive_archery_ue4.h"
 #endif//__AUTO_VIVE_ARCHERY_UE4_H__
