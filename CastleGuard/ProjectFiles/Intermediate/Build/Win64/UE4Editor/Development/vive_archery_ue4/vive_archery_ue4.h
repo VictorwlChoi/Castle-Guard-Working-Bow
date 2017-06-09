608f22d0160d6f65b86178783fd97db28b5ca15d
VictorwlChoi <vncvrvictorchoi@outlook.com>
2017-06-08 22:03:34 -0700

    Health Bar UI and Functionality added to Catapult

--- CastleGuard/ProjectFiles/Intermediate/Build/Win64/UE4Editor/Development/vive_archery_ue4/vive_archery_ue4.h
+++ CastleGuard/ProjectFiles/Intermediate/Build/Win64/UE4Editor/Development/vive_archery_ue4/vive_archery_ue4.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_VIVE_ARCHERY_UE4_H__
 #define __AUTO_VIVE_ARCHERY_UE4_H__
-//Last Write: 6/5/2017 9:09:37 PM +00:00
-#include "E:\Castle-Guard-Working-Bow\CastleGuard\vive_archery_ue4-master 4.13\Source\vive_archery_ue4\vive_archery_ue4.h"
+//Last Write: 6/9/2017 2:29:03 AM +00:00
+#include "E:\CGLD\CGLDRevert\Castle-Guard-Working-Bow\CastleGuard\ProjectFiles\Source\vive_archery_ue4\vive_archery_ue4.h"
 #endif//__AUTO_VIVE_ARCHERY_UE4_H__
