3ec6a0032bea3023fb50f43d7b56ccc85aa22041
Maccabee <Maccabee>
2017-06-10 22:52:33 -0700

    Adjusting HMD Vector to account for cover

--- CastleGuard/ProjectFiles/Intermediate/Build/Win64/UE4Editor/Development/vive_archery_ue4/vive_archery_ue4.h
+++ CastleGuard/ProjectFiles/Intermediate/Build/Win64/UE4Editor/Development/vive_archery_ue4/vive_archery_ue4.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_VIVE_ARCHERY_UE4_H__
 #define __AUTO_VIVE_ARCHERY_UE4_H__
-//Last Write: 6/11/2017 1:41:38 AM +00:00
-#include "D:\CastleGuardLoneDefender\Castle-Guard-Working-Bow\CastleGuard\ProjectFiles\Source\vive_archery_ue4\vive_archery_ue4.h"
+//Last Write: 6/11/2017 5:23:48 AM +00:00
+#include "E:\CastleGuardLoneDefender\Castle-Guard-Working-Bow\CastleGuard\ProjectFiles\Source\vive_archery_ue4\vive_archery_ue4.h"
 #endif//__AUTO_VIVE_ARCHERY_UE4_H__
